91d59c454ed0fa94b3ccf41d549913a38dec495c
Elias Lima <sci.eliaslima@gmail.com>
2023-02-22 00:10:40 -0300

    chore: app before atomic design

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import ProfileSvg from '../assets/icons/profile.svg';
+import SearchSvg from '../assets/icons/search.svg';
+import VideoSvg from '../assets/icons/video.svg';
+import HomeSvg from '../assets/icons/home.svg';
+import BagSvg from '../assets/icons/bag.svg';
+
+import { Feed } from '../pages/Feed';
+
+const AppTab = createBottomTabNavigator();
+
+const TabRoutes = () => {
+  return (
+    <AppTab.Navigator screenOptions={{headerShown: false}}>
+      <AppTab.Screen
+        name="Home"
+        component={Feed}
+        options={{
+          tabBarIcon: (() => (
+            <HomeSvg />
+          ))
+        }}
+      />
+      <AppTab.Screen
+        name="Search"
+        component={Feed}
+        options={{
+          tabBarIcon: (() => (
+            <SearchSvg />
+          ))
+        }}
+      />
+      <AppTab.Screen
+        name="New"
+        component={Feed}
+        options={{
+          tabBarIcon: (() => (
+            <VideoSvg />
+          ))
+        }}
+      />
+      <AppTab.Screen
+        name="Favorite"
+        component={Feed}
+        options={{
+          tabBarIcon: (() => (
+            <BagSvg />
+          ))
+        }}
+      />
+      <AppTab.Screen
+        name="Profile"
+        component={Feed}
+        options={{
+          tabBarIcon: (() => (
+            <ProfileSvg />
+          ))
+        }}
+      />
+    </AppTab.Navigator>
+  )
+}
+
+export default TabRoutes;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import TabRoutes from './app.routes';
+
+
+const Routes = () => (
+  <NavigationContainer>
+    <TabRoutes />
+  </NavigationContainer>
+)
+
+export default Routes;
